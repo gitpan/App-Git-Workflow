89f7f4d68b2f18678162ada1bc23f57029b5dbe7
Ivan Wills <ivanwills@cpan.org>
2014-10-29 20:49:11 +0000

    Import of IVANWILLS/App-Git-Workflow-0.9 from CPAN.
    gitpan-cpan-distribution: App-Git-Workflow
    gitpan-cpan-version:      0.9
    gitpan-cpan-path:         IVANWILLS/App-Git-Workflow-0.9.tar.gz
    gitpan-cpan-author:       IVANWILLS
    gitpan-cpan-maturity:     released

--- lib/Test/Git/Workflow/Command.pm
+++ lib/Test/Git/Workflow/Command.pm
@@ -17,7 +17,7 @@ use Test::MockTime qw/restore_time set_fixed_time/;
 use App::Git::Workflow;
 use Mock::App::Git::Workflow::Repository;
 
-our $VERSION     = 0.8;
+our $VERSION     = 0.9;
 our @EXPORT      = qw/command_ok/;
 our @EXPORT_OK   = qw/command_ok/;
 our %EXPORT_TAGS = ();
@@ -121,93 +121,77 @@ __END__
 
 =head1 NAME
 
-Test::Git::Workflow::Command - <One-line description of module's purpose>
+Test::Git::Workflow::Command - Test Git::Workflow::Command::* files
 
 =head1 VERSION
 
-This documentation refers to Test::Git::Workflow::Command version 0.8
-
+This documentation refers to Test::Git::Workflow::Command version 0.9
 
 =head1 SYNOPSIS
 
    use Test::Git::Workflow::Command;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
-
+   command_ok('Test::Git::Workflow::Command::SomeCommand', {...});
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Helper module to test L<Git::Worflow::Commands>s
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head3 C<command_ok ( $module, $data )>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+Tests C<$module> with the supplied <C$data>
 
-Name the section accordingly.
+C<$data> keys
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
+=over 4
 
+=item ARGV
 
-=head3 C<new ( $search, )>
+The commands command line input
 
-Param: C<$search> - type (detail) - description
+=item mock
 
-Return: Test::Git::Workflow::Command -
+The mock data to supply to the L<Mock::App::Git::Workflow::Repository> object
 
-Description:
+=item STD
 
-=cut
+=over 4
 
+=item IN
+
+STDIN
+
+=item OUT
+
+STDOUT
+
+=item ERR
+
+STDERR
+
+=back
+
+=item option
+
+What the C<%option>s hash should contain at the end of everything
+
+=item name
+
+Name of the test
+
+=back
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -218,7 +202,6 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
